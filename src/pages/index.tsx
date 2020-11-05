802d1cde17f586db8e57620548b3ad44093daab4
Todd Christensen <todd@Todds-MacBook-Pro.local>
2020-11-05 11:11:46 +0800

    can create recipe with auth and fetch all recipes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,17 @@ import { GetServerSideProps } from "next";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { loginWithGithub, getAuth, logout } from "../client/firebaseHelpers";
 import { Header } from "../client/components/Header";
-import { useCurrentUserQuery, useDummyMutation } from "../client/gen/index";
+import {
+  useCurrentUserQuery,
+  useDummyMutation,
+  useCreateRecipeWithIngredientsMutation,
+} from "../client/gen/index";
 import { GraphQLBoolean } from "graphql";
+
+// import { gql, useMutation } from '@apollo/client';
+import { useMutation, useQuery } from "@apollo/react-hooks";
+import gql from "graphql-tag";
+import React from "react";
 
 type Props = {};
 
@@ -15,7 +24,7 @@ export const getServerSideProps: GetServerSideProps<Props> = async (ctx) => {
 
 const Index = () => {
   const [user, loading] = useAuthState(getAuth());
-  console.log('user:', user);
+  // console.log('user:', user);
   return (
     <main>
       <Header />
@@ -27,10 +36,112 @@ const Index = () => {
       ) : (
         <button onClick={() => loginWithGithub()}>Login</button>
       )}
+      <CreateRecipe />
+      <GetRecipes />
     </main>
   );
 };
 
+const GET_RECIPES = gql`
+  query {
+    Recipe {
+      recipeId
+      name
+    }
+  }
+`;
+
+ type CustomIngredientsInput = {
+   amount: number;
+   measurement: string;
+   flavorId: string;
+ }
+
+const CREATE_RECIPE_WITH_INGREDIENTS = gql`
+  mutation createRecipeWithIngredients(
+    $userId: String!
+    $recipeId: ID!
+    $name: String!
+    $description: String!
+    $published: String!
+    $isArchived: Boolean!
+    $ingredients: [CustomIngredientsInput]
+  ) {
+    createRecipeWithIngredients(
+      userId: $userId
+      recipeId: $recipeId
+      name: $name
+      description: $description
+      published: $published
+      isArchived: $isArchived
+      ingredients: $ingredients
+    ) {
+      name
+      recipeId
+    }
+  }
+`;
+
+function CreateRecipe() {
+  const [CreateRecipeWithIngredients] = useMutation(
+    CREATE_RECIPE_WITH_INGREDIENTS,
+    {
+      onCompleted: (res) => {
+        console.log(res);
+      },
+      onError: (err) => {
+        console.error(err);
+      },
+    }
+  );
+
+  const RecipePayload = {
+    variables: {
+      userId: "google-oauth2|109167584921949758779",
+      recipeId: "111123-recipe-id-123weewe",
+      name: "123ABCDEFG",
+      description: "Recipe description",
+      published: "2020-09-22T16:17:31.419Z",
+      isArchived: false,
+      ingredients: [
+        {
+          amount: 10,
+          measurement: "%",
+          flavorId: "4c09d545-9e25-42fa-8173-fa7f6529611d",
+        },
+      ],
+    },
+  };
+
+
+
+  return (
+    <div>
+      <button
+        onClick={() => {
+          CreateRecipeWithIngredients(RecipePayload);
+        }}
+      >
+        Create Recipe
+      </button>
+    </div>
+  );
+}
+
+function GetRecipes() {
+  const { loading, data, error } = useQuery(GET_RECIPES);
+
+  return (
+    <div style={{ marginTop: "30px" }}>
+      {loading && !error && <p>Loading...</p>}
+      {error && !loading && <p>Error: {JSON.stringify(error)}</p>}
+      {data &&
+        !loading &&
+        !error &&
+        data.Recipe.map((recipe: any) => <li>{recipe.name}</li>)}
+    </div>
+  );
+}
 
 function GraphqlExample() {
   const currentUserQuery = useCurrentUserQuery();
@@ -42,7 +153,7 @@ function GraphqlExample() {
         disabled={result.loading}
         onClick={async () => {
           const res = await dummy({ variables: {} });
-          console.log(res.data?.dummy?.error);
+          alert(res.data?.dummy?.error);
         }}
       >
         run command
--- src/server/graphqlResolver.ts
+++ src/server/graphqlResolver.ts
@@ -5,29 +5,29 @@ import { neo4jgraphql } from "neo4j-graphql-js";
 type Context = { idToken: { uid: string } | null };
 
 const Query: QueryResolvers<Context> = {
+  
   async currentUser(_parent, _args, context, _info) {
     return {
       id: context.idToken?.uid!,
     };
   },
+
   //@ts-ignore
   Recipe(object, params, _context, resolveInfo) {
-    if (_context.idToken?.uid == null) {
-      throw new Error("need auth");
-    }
     return neo4jgraphql(object, params, _context, resolveInfo);
   },
 };
 
 
 const Mutation: MutationResolvers<Context> = {
+
   //@ts-ignore
-  // async CreateUser(object, params, _context, resolveInfo) {
-  //   if (_context.idToken?.uid == null) {
-  //     throw new Error("need auth");
-  //   }
-  //   return neo4jgraphql(object, params, _context, resolveInfo);
-  // },
+  async createRecipeWithIngredients(object, params, _context, resolveInfo) {
+    if (_context.idToken?.uid == null) {
+      throw new Error("need auth");
+    }
+    return neo4jgraphql(object, params, _context, resolveInfo);
+  },
 
   async dummy(_parent, _args, _context, _info) {
     if (_context.idToken?.uid == null) {
