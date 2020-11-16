a897eb404d2b5edb31e36e2e226facbc70a30cd9
Todd Christensen <todd@Todds-MacBook-Pro.local>
2020-11-16 11:37:51 +0800

    basically turned the sidebar into twitter

--- src/pages/recipes.tsx
+++ src/pages/recipes.tsx
@@ -2,6 +2,11 @@ import React from "react";
 import { GetServerSideProps } from "next";
 import Link from "next/link";
 import { useMutation, useQuery } from "@apollo/react-hooks";
+import { useAuthState } from "react-firebase-hooks/auth";
+import {
+  getAuth,
+  logout,
+} from "../client/firebaseHelpers";
 import gql from "graphql-tag";
 import { Header } from "../client/components/Header";
 
@@ -21,6 +26,7 @@ import {
   MenuList,
   MenuDivider,
   Icon,
+  Heading
 } from "@chakra-ui/core";
 import {
   BiChevronDown,
@@ -29,6 +35,7 @@ import {
   BiNotification,
   BiBookmark,
   BiHash,
+  BiUser
 } from "react-icons/bi";
 
 import { formatDistanceToNow } from "date-fns";
@@ -257,7 +264,7 @@ function RecipeCard(recipe: Recipe) {
 }
 
 const GetRecipes = () => {
-  const { loading, data, error } = useQuery(RECIPES_NOT_ARCHIVED, {
+  const recipes = useQuery(RECIPES_NOT_ARCHIVED, {
     notifyOnNetworkStatusChange: true,
     variables: {
       orderBy: "published_desc",
@@ -276,12 +283,12 @@ const GetRecipes = () => {
           marginRight: "auto",
         }}
       >
-        {loading && !error && <p>Loading...</p>}
-        {error && !loading && <p>Error: {JSON.stringify(error)}</p>}
-        {data &&
-          !loading &&
-          !error &&
-          data.recipesNotArchived.map((recipe: any) => (
+        {recipes.loading && !recipes.error && <p>Loading...</p>}
+        {recipes.error && !recipes.loading && <p>Error: {JSON.stringify(recipes.error)}</p>}
+        {recipes.data &&
+          !recipes.loading &&
+          !recipes.error &&
+          recipes.data.recipesNotArchived.map((recipe: any) => (
             <RecipeCard key={recipe.recipeId} {...recipe} />
           ))}
       </div>
@@ -290,87 +297,166 @@ const GetRecipes = () => {
 };
 
 const RecipesPage = () => {
+  const [user, loading] = useAuthState(getAuth());
   return (
     <Box>
-      <Header />
+      {/* <Header /> */}
       <Box display="flex" justifyContent="center" pt={8}>
-        <Box id="sidebar-left" display="flex" flexGrow="1" alignItems="flex-end" flexDir="column">
-          <Box position="fixed" top="100px">
-            <Stack direction={"column"} spacing={5} align="stretch">
-              <Box>
-                <Link href="/">
-                  <a>
-                    <Box
-                      display="flex"
-                      flexDir="row"
-                      alignItems="center"
-                      justifyContent="flex-start"
-                    >
-                      <Icon w={10} h={10} color="gray.600" as={BiHome} />
-                      <Text ml={2} fontWeight="bold">
-                        Home
-                      </Text>
-                    </Box>
-                  </a>
-                </Link>
-              </Box>
-              <Box>
-                <Link href="/">
-                  <a>
-                    <Box
-                      display="flex"
-                      flexDir="row"
-                      alignItems="center"
-                      justifyContent="flex-start"
-                    >
-                      <Icon w={10} h={10} color="gray.600" as={BiHash} />
-                      <Text ml={2} fontWeight="bold">
-                        Explore
-                      </Text>
-                    </Box>
-                  </a>
-                </Link>
-              </Box>
-              <Box>
-                <Link href="/">
-                  <a>
-                    <Box
-                      display="flex"
-                      flexDir="row"
-                      alignItems="center"
-                      justifyContent="flex-start"
-                    >
-                      <Icon
-                        w={10}
-                        h={10}
-                        color="gray.600"
-                        as={BiNotification}
-                      />
-                      <Text ml={2} fontWeight="bold">
-                        Notifications
-                      </Text>
-                    </Box>
-                  </a>
-                </Link>
-              </Box>
-              <Box>
-                <Link href="/">
-                  <a>
-                    <Box
-                      display="flex"
-                      flexDir="row"
-                      alignItems="center"
-                      justifyContent="flex-start"
-                    >
-                      <Icon w={10} h={10} color="gray.600" as={BiBookmark} />
-                      <Text ml={2} fontWeight="bold">
-                        Bookmarks
-                      </Text>
-                    </Box>
-                  </a>
-                </Link>
-              </Box>
-            </Stack>
+        <Box
+          id="sidebar-left"
+          display="flex"
+          flexGrow="1"
+          alignItems="flex-end"
+          flexDir="column"
+        >
+          <Box w="175px" maxW="100%">
+            <Box id="sidebar-left__top" position="fixed" top="40px">
+              <Heading as="h1" size="lg" letterSpacing={"-.1rem"}>
+                JuiceSauce
+              </Heading>
+            </Box>
+            <Box id="sidebar-left__main" position="fixed" top="100px">
+              <Stack direction={"column"} spacing={6} align="stretch">
+                <Box>
+                  <Link href="/">
+                    <a>
+                      <Box
+                        display="flex"
+                        flexDir="row"
+                        alignItems="center"
+                        justifyContent="flex-start"
+                      >
+                        <Icon w={10} h={10} color="gray.600" as={BiHome} />
+                        <Text ml={2} fontWeight="bold">
+                          Home
+                        </Text>
+                      </Box>
+                    </a>
+                  </Link>
+                </Box>
+                <Box>
+                  <Link href="/explore">
+                    <a>
+                      <Box
+                        display="flex"
+                        flexDir="row"
+                        alignItems="center"
+                        justifyContent="flex-start"
+                      >
+                        <Icon w={10} h={10} color="gray.600" as={BiHash} />
+                        <Text ml={2} fontWeight="bold">
+                          Explore
+                        </Text>
+                      </Box>
+                    </a>
+                  </Link>
+                </Box>
+                <Box>
+                  <Link href="/notifications">
+                    <a>
+                      <Box
+                        display="flex"
+                        flexDir="row"
+                        alignItems="center"
+                        justifyContent="flex-start"
+                      >
+                        <Icon
+                          w={10}
+                          h={10}
+                          color="gray.600"
+                          as={BiNotification}
+                        />
+                        <Text ml={2} fontWeight="bold">
+                          Notifications
+                        </Text>
+                      </Box>
+                    </a>
+                  </Link>
+                </Box>
+                <Box>
+                  <Link href="/bookmarks">
+                    <a>
+                      <Box
+                        display="flex"
+                        flexDir="row"
+                        alignItems="center"
+                        justifyContent="flex-start"
+                      >
+                        <Icon w={10} h={10} color="gray.600" as={BiBookmark} />
+                        <Text ml={2} fontWeight="bold">
+                          Bookmarks
+                        </Text>
+                      </Box>
+                    </a>
+                  </Link>
+                </Box>
+                {!loading && user && (
+                  <Box>
+                    <Link href={`/u/${user.uid}`}>
+                      <a>
+                        <Box
+                          display="flex"
+                          flexDir="row"
+                          alignItems="center"
+                          justifyContent="flex-start"
+                        >
+                          <Icon w={10} h={10} color="gray.600" as={BiUser} />
+                          <Text ml={2} fontWeight="bold">
+                            Profile
+                          </Text>
+                        </Box>
+                      </a>
+                    </Link>
+                  </Box>
+                )}
+              </Stack>
+            </Box>
+            <Box id="sidebar-left__bottom" position="fixed" bottom="40px">
+              {!loading && user ? (
+                <Box display="flex">
+                  <Box display="flex" alignItems="center">
+                    <Menu>
+                      <MenuButton
+                        // px={4}
+                        py={2}
+                        transition="all 0.2s"
+                        borderRadius="md"
+                        // _hover={{ bg: "gray.100" }}
+                        _focus={{ outline: 0, boxShadow: "outline" }}
+                        bg="transparent"
+                        display="flex"
+                        justifyContent="center"
+                        alignItems="center"
+                      >
+                        <Avatar
+                          marginRight={2}
+                          size="sm"
+                          name={user.displayName}
+                          src={user.photoURL}
+                        />
+                        <Text>{user.displayName}</Text>
+                        <Icon as={BiChevronDown} />
+                      </MenuButton>
+                      <MenuList color="gray.500">
+                        <MenuItem>
+                          <Link href="/dashboard">
+                            <a>Dashboard</a>
+                          </Link>
+                        </MenuItem>
+                        <MenuDivider />
+                        <MenuItem onClick={() => logout()}>Logout</MenuItem>
+                      </MenuList>
+                    </Menu>
+                  </Box>
+                </Box>
+              ) : (
+                <Box>
+                  <Link href="/login">
+                    <a>Login</a>
+                  </Link>
+                </Box>
+              )}
+            </Box>
           </Box>
         </Box>
         <Box id="feed" px={6}>
@@ -378,7 +464,13 @@ const RecipesPage = () => {
 
           <GetRecipes />
         </Box>
-        <Box id="sidebar-right" display="flex" flexGrow="1" alignItems="flex-end" flexDir="column"></Box>
+        <Box
+          id="sidebar-right"
+          display="flex"
+          flexGrow="1"
+          alignItems="flex-end"
+          flexDir="column"
+        ></Box>
       </Box>
     </Box>
   );
